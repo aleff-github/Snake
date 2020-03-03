9611a8336595df4a3701de74cdd938b581fe9be8
nonameon <nonameon@protonmail.ch>
2020-03-03 12:03:16 +0100

    Update snake.cpp

--- snake.cpp
+++ snake.cpp
@@ -2,17 +2,15 @@
 using namespace std;
 const unsigned int secondsToWait = 100000;
 int main (){
-	
-	game_board snakeGame;
-	snakeGame.start();
-
-	while(!snakeGame.getGameOver()){
-		usleep(secondsToWait);
-		snakeGame.draw();
-		snakeGame.command();
-		snakeGame.logic();
+	game_board snakeGame; //Costructor default -> Snake
+	snakeGame.start(); //Set pacman function && nickname gamer
+	while(!snakeGame.getGameOver()){ //gameover is a boolean that control if you lose
+		usleep(secondsToWait); //Wait 10 seconds before start
+		snakeGame.draw(); //Draw metod - print score, matrix, snake(with the tail) and fruit
+		snakeGame.command(); //Use kbhit and getch to take an input command - Watch 'logic()' metod
+		snakeGame.logic(); //Tail system - Logic system
 	}
 	
-	snakeGame.finish();
+	snakeGame.finish(); //End program - Use 'toilet' to print "You win!" or "You lose!"
 	
 }
